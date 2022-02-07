662010bd637148db588fe5d52e7ead92bb5773ff
Estiven Betancur Palacio <esbet024@hotmail.com>
2022-02-07 18:51:08 -0500

    App Terminada

---
+++ 01_band_name/lib/services/socket_service.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/cupertino.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+enum ServerStatus { Online, Offline, Connecting }
+
+class SocketService with ChangeNotifier {
+  ServerStatus _serverStatus = ServerStatus.Connecting;
+  late IO.Socket _socket;
+
+  ServerStatus get serverStatus => _serverStatus;
+
+  IO.Socket get socket => _socket;
+  Function get emit => _socket.emit;
+
+  SocketService() {
+    _initConfig();
+  }
+
+  void _initConfig() {
+    // Dart client
+
+    _socket = IO.io('http://192.168.1.6:3000', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': true,
+    });
+
+    _socket.on('connect', (_) {
+      _serverStatus = ServerStatus.Online;
+      notifyListeners();
+    });
+
+    _socket.on('disconnect', (_) {
+      _serverStatus = ServerStatus.Offline;
+      notifyListeners();
+    });
+
+   
+  }
+}
